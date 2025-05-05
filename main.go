67000f03e001a29cccf86bb1e5d69d2813cfc45e
Myster1o619 <myster1o619@hotmail.com>
2025-05-05 12:41:14 +0200

    setup basic router

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import(
+import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "hello!",
+	})
+}
+
 func main() {
 	router := gin.Default()
+
+	router.GET("/events", getEvents)
+
 	router.Run(":8080")
 }
