e2bd4ab7f23c7a3e5a21c3a1b2f3e79f481d37e6
Myster1o619 <myster1o619@hotmail.com>
2025-05-06 10:56:50 +0200

    update Save()

--- models/event.go
+++ models/event.go
@@ -4,22 +4,47 @@ package models
 
 import (
 	"time"
+
+	"example.com/rest_api/db"
 )
 
 type Event struct {
-	ID          int
-	Name        string `binding:"required"`
-	Description string `binding:"required"`
-	Location    string `binding:"required"`
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
 	Date        time.Time `binding:"required"`
 	UserID      int
 }
 
 var events = []Event{}
 
-func (evt *Event) Save() {
-	// later: save to database
-	events = append(events, *evt)
+func (evt *Event) Save() error {
+	query := `
+	INSERT INTO TABLE events(name, description, location, date, user_id) 
+	VALUES(?, ?, ?, ?, ?)
+	`
+	statement, err := db.SqlDatabase.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := statement.Exec(evt.Name, evt.Description, evt.Location, evt.Date, evt.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return err
+	}
+
+	evt.ID = id
+	
+	return nil
 }
 
 func GetAllEvents() []Event {
