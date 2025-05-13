9501cff475f5af2d3f1d0ec245b1fcba12d25445
Myster1o619 <myster1o619@hotmail.com>
2025-05-13 11:13:15 +0200

    add Save method to User struct

--- models/user.go
+++ models/user.go
@@ -9,3 +9,29 @@ type User struct {
 	Email    string `binding:"required"`
 	Password string `binding:"required"`
 }
+
+func (usr *User) Save() error {
+	query := `
+	INSERT INTO users(id, email, password) 
+	VALUES(?, ?, ?)
+	`
+	statement, err := db.SqlDatabase.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer statement.Close()
+
+	result, err := statement.Exec(usr.ID, usr.Email, usr.Password)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	usr.ID = id
+
+	return err
+}
