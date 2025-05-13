6b42045314141f41df8e5cb1db01de02d5574d28
Myster1o619 <myster1o619@hotmail.com>
2025-05-13 11:14:42 +0200

    add createUser function

---
+++ routes/users.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	// "strconv"
+
+	"example.com/rest_api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func createUser(context *gin.Context) {
+	var usr models.User
+	err := context.ShouldBindJSON(&usr)
+
+	if err != nil {
+		errString := fmt.Sprintf("Unable to create user: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": errString,
+		})
+		return
+	}
+
+	// dummy data
+	// event.ID = 1
+	// event.UserID = 1
+
+	err = usr.Save()
+
+	if err != nil {
+		errString := fmt.Sprintf("Unable to create user: %v", err)
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": errString,
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "user created successfully",
+		"event":   usr,
+	})
+}
