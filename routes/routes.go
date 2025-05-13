4580d24608eafa5674b7b49d396da53371913fe2
Myster1o619 <myster1o619@hotmail.com>
2025-05-13 10:47:34 +0200

    cleanup and prepare for user routes functionality

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,12 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(router *gin.Engine) {
-
+	// event routes
 	router.GET("/events", getEvents)
 	router.GET("/events/:id", getEventByID)
-
 	router.POST("/events", createEvent)
-
 	router.PUT("/events/:id", updateEvent)
+	router.DELETE("/events/:id", deleteEvent)
+
+	// user routes
 }
