6f40643ddf7868b3a5393ece8f235eeb16660682
Myster1o619 <myster1o619@hotmail.com>
2025-05-16 09:02:23 +0200

    add HashPassword() function

---
+++ utils/hash.go
@@ -0,0 +1,8 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(passwrd string) (string, error) {
+	bytePass, err := bcrypt.GenerateFromPassword([]byte(passwrd), bcrypt.DefaultCost)
+	return string(bytePass), err
+}
