535f1a034d4cf17cb48863566d597a0fdb83240d
Myster1o619 <myster1o619@hotmail.com>
2025-05-29 10:23:02 +0200

    update Event struct

--- models/event.go
+++ models/event.go
@@ -12,7 +12,7 @@ type Event struct {
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
 	Date        time.Time `binding:"required"`
-	UserID      int
+	UserID      int64
 }
 
 func (evt *Event) Save() error {
