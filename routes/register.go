4b9d33635ba0eba30e971a655906a868812ab328
Myster1o619 <myster1o619@hotmail.com>
2025-05-30 10:31:00 +0200

    add registerForEvent logic

--- routes/register.go
+++ routes/register.go
@@ -1,9 +1,52 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"example.com/rest_api/models"
+	"github.com/gin-gonic/gin"
+)
 
 func registerForEvent(context *gin.Context) {
+	usrID := context.GetInt64("usrID")
 
+	stringEvtID := context.Param("id")
+	eventID, err := strconv.ParseInt(stringEvtID, 10, 64)
+
+	if err != nil {
+		errString := fmt.Sprintf("Error converting ID %v to integer: %v", stringEvtID, err)
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": errString,
+		})
+		return
+	}
+
+	event, err := models.GetEvent(eventID)
+
+	if err != nil {
+		errString := fmt.Sprintf("Unable to retrieve event: %v", err)
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": errString,
+		})
+		return
+	}
+
+	err = event.Register(usrID)
+
+	if err != nil {
+		errString := fmt.Sprintf("Unable to register event: %v", err)
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message": errString,
+		})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{
+		"message": "Event registered successfully",
+		"event":   event,
+	})
 }
 
 func cancelRegistration(context *gin.Context) {
